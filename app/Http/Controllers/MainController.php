1f6929d9882ef50417fefcb97e525e7c10ec0ae8
misha <dybrovskij2001@gmail.comsudo su>
2021-01-23 00:54:37 +0200

    Controllers rout

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function categories()
+    {
+        $categories=Category::get();
+        return view('categories',compact('categories'));
+    }
+
+    public function product($product = null)
+    {
+        //dump($product);
+        //or
+        //dd{$product);
+        return view('product', ['product' => $product]);
+    }
+
+    public function category($code)
+    {
+        $category = Category::where('code',$code)->first();
+        return view('category',compact('category'));
+//     dd($category);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\MainController::class,'index']);
+Route::get('/categories', [\App\Http\Controllers\MainController::class,'categories']);
+Route::get('/{category}',[\App\Http\Controllers\MainController::class,'category']);
+Route::get('/mobiles/{product?}', [\App\Http\Controllers\MainController::class,'product']);
+
+//Route::get('/categories',function (){
+//   return view('categories');
+//});
+//Route::get('/mobiles/iphone_x_64', function () {
+//    return view('product');
+//});
